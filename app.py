52f07e9809bb32a8e4f84933b6eecb78e9c0ecbc
impanaj07 <impanajgowda14@gmail.com>
2024-03-11 22:01:51 +0530

    update:added download feature

--- app.py
+++ app.py
@@ -1,51 +1,66 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,send_file
+import requests
+import os
+import io
+from base64 import b64encode
+from PIL import Image
 
-from transformers import PegasusForConditionalGeneration, PegasusTokenizer
-import torch
-
+API_KEY = "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"
+TEXT_URL = "https://api-inference.huggingface.co/models/google/pegasus-large"
+IMAGE_URL = "https://api-inference.huggingface.co/models/nerijs/pixel-art-xl"
 app = Flask(__name__)
-
-model_name = "google/pegasus-xsum"
-tokenizer = PegasusTokenizer.from_pretrained(model_name)
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model = PegasusForConditionalGeneration.from_pretrained(model_name).to(device)
-
-@app.route('/')
+directory_path = 'uploads'
+if not os.path.exists(directory_path):
+    os.makedirs(directory_path)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+@app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route('/text-summarization', methods=["POST"])
+@app.route("/image_generation")
+def imager():
+    return render_template("image_generation.html")
+
+
+
+
+@app.route("/text-summarization", methods=["POST"])
 def summarize():
 
     if request.method == "POST":
 
         inputtext = request.form["inputtext_"]
+        payload = {"inputs": inputtext}
+        response = requests.post(TEXT_URL, headers=headers, json=payload)
+        summary_json = response.json()
+        summary = summary_json[0]["summary_text"]
+        return render_template("output.html", data={"summary": summary})
 
-        input_text = "summarize: " + inputtext
+@app.route("/text-to-pixel-art", methods=["POST"])
+def query():
+    if request.method == "POST":
+        headers = {"Authorization": f"Bearer {API_KEY}"}
+        text = request.form["imageinput"]
+        payload = {"inputs": f"pixel art,{text}"}
+        response = requests.post(IMAGE_URL, headers=headers, json=payload)
+        image_bytes = response.content
+        image = Image.open(io.BytesIO(image_bytes))
+        buffered = io.BytesIO()
+        image.save(buffered, format="PNG")
+        dataurl = "data:image/png;base64," + b64encode(buffered.getvalue()).decode(
+            "ascii"
+        )
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], 'generated_image.png')
+        image.save(image_path)
 
-        tokenized_text = tokenizer.encode(input_text, return_tensors='pt', max_length=512).to(device)
-        summary_ = model.generate(tokenized_text, min_length=30, max_length=500)
-        summary = tokenizer.decode(summary_[0], skip_special_tokens=True)
-
-        '''
-            text = <start> i am yash <end>
-            vocab = { i: 1, am : 2, yash: 3, start 4}
-
-            token = [i, am ,yash]
-            encode = [1 2, 3, 4]
-
-            summary_ = [[4, 3,1, 5]]
-
-            summary = yash i
-
-        
-        '''
-
-    return render_template("output.html", data = {"summary": summary})
-
-if __name__ == '__main__':
-    app.run(host="192.168.1.12",port="5000")
+        return render_template("output.html", data={"image": dataurl})
+@app.route("/download_image")
+def download_image():
+    image_path = os.path.join(app.config['UPLOAD_FOLDER'], 'generated_image.png')
+    return send_file(image_path, as_attachment=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="4000")
 
 
 # incase of error use post as 0.0.0.0
